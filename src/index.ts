ChainSafe/lodestar

update bls methods and wasm usage

packages/bls/src/index.ts
@@ -45,11 +45,12 @@ export function sign(secretKey: BLSSecretKey, messageHash: bytes32): BLSSignatur
  */
 export function aggregateSignatures(signatures: BLSSignature[]): BLSSignature {
   assert(signatures, "signatures is null or undefined");
-  return signatures.map((signature): Signature => {
-    return Signature.fromCompressedBytes(signature);
-  }).reduce((previousValue, currentValue): Signature => {
-    return previousValue.add(currentValue);
-  }).toBytesCompressed();
+
+  return Signature.aggregate(
+    signatures.map((signature): Signature => {
+      return Signature.fromCompressedBytes(signature);
+    })
+  ).toBytesCompressed();
 }
 
 /**
@@ -91,15 +92,37 @@ export function verify(publicKey: BLSPubkey, messageHash: bytes32, signature: BL
 }
 
 /**
+ * Verifies if aggregated signature is same message signed with given public keys.
+ * @param publicKeys
+ * @param messageHash
+ * @param signature
+ */
+export function verifyAggregate(publicKeys: BLSPubkey[], messageHash: bytes32, signature: BLSSignature): boolean {
+  assert(publicKeys, "publicKey is null or undefined");
+  assert(messageHash, "messageHash is null or undefined");
+  assert(signature, "signature is null or undefined");
+  try {
+    return Signature
+      .fromCompressedBytes(signature)
+      .verifyAggregate(publicKeys, messageHash);
+  } catch (e) {
+    console.log(e)
+    return false;
+  }
+}
+
+/**
  * Verifies if signature is list of message signed with corresponding public key.
  * @param publicKeys
  * @param messageHashes
  * @param signature
+ * @param fast Check if all messages are different
  */
 export function verifyMultiple(
   publicKeys: BLSPubkey[],
   messageHashes: bytes32[],
-  signature: BLSSignature
+  signature: BLSSignature,
+  fast = false
 ): boolean {
   assert(publicKeys, "publicKey is null or undefined");
   assert(messageHashes, "messageHash is null or undefined");
@@ -113,7 +136,8 @@ export function verifyMultiple(
       .fromCompressedBytes(signature)
       .verifyMultiple(
         publicKeys.map((key) => PublicKey.fromBytes(key)),
-        messageHashes
+        messageHashes,
+        fast
       );
   } catch (e) {
     return false;
@@ -127,5 +151,6 @@ export default {
   aggregateSignatures,
   aggregatePubkeys,
   verify,
+  verifyAggregate,
   verifyMultiple
 };

packages/bls/src/signature.ts
@@ -1,6 +1,6 @@
 import assert from "assert";
 import {FP_POINT_LENGTH} from "./constants";
-import {BLSSignature, bytes32} from "@chainsafe/eth2.0-types";
+import {BLSPubkey, BLSSignature, bytes, bytes32} from "@chainsafe/eth2.0-types";
 import {SignatureType} from "@chainsafe/eth2-bls-wasm";
 import {getContext} from "./context";
 import {PublicKey} from "./publicKey";
@@ -12,6 +12,7 @@ export class Signature {
 
   protected constructor(value: SignatureType) {
     this.value = value;
+    assert(this.value.isValidOrder());
   }
 
   public static fromCompressedBytes(value: BLSSignature): Signature {
@@ -31,6 +32,15 @@ export class Signature {
     return new Signature(signature);
   }
 
+  public static aggregate(signatures: Signature[]): Signature {
+    const context = getContext();
+    const signature = new context.Signature();
+    signature.aggregate(signatures.map((sig) => sig.getValue()));
+    return new Signature(
+      signature
+    );
+  }
+
   public add(other: Signature): Signature {
     const agg = this.value.clone();
     agg.add(other.value);
@@ -43,14 +53,21 @@ export class Signature {
     return this.value;
   }
 
-  public verify(publicKey: PublicKey, message: bytes32): boolean {
-    return publicKey.verifyMessage(this, message);
+  public verifyAggregate(publicKey: BLSPubkey[], message: bytes32): boolean {
+    return this.value.fastAggregateVerify(
+      publicKey.map((bytes) => PublicKey.fromBytes(bytes).getValue()),
+      message
+    );
   }
 
-  public verifyMultiple(publicKeys: PublicKey[], messages: bytes32[]): boolean {
-    return this.value.verifyAggregatedHashWithDomain(
+  public verifyMultiple(publicKeys: PublicKey[], messages: bytes32[], fast = false): boolean {
+    const msgs = Buffer.concat(messages);
+    if(!fast && !getContext().areAllMsgDifferent(msgs)) {
+      return false;
+    }
+    return this.value.aggregateVerifyNoCheck(
       publicKeys.map((key) => key.getValue()),
-      messages
+      msgs
     );
   }
 

ChainSafe/bls

src/index.ts
@@ -48,11 +48,11 @@ export function sign(secretKey: Uint8Array, messageHash: Uint8Array): Buffer {
  */
 export function aggregateSignatures(signatures: Uint8Array[]): Buffer {
   assert(signatures, "signatures is null or undefined");
-  return signatures.map((signature): Signature => {
-    return Signature.fromCompressedBytes(toBuffer(signature));
-  }).reduce((previousValue, currentValue): Signature => {
-    return previousValue.add(currentValue);
-  }).toBytesCompressed();
+  return Signature.aggregate(
+    signatures.map((signature): Signature => {
+      return Signature.fromCompressedBytes(signature);
+    })
+  ).toBytesCompressed();
 }
 
 /**
@@ -90,15 +90,36 @@ export function verify(publicKey: Uint8Array, messageHash: Uint8Array, signature
 }
 
 /**
+ * Verifies if aggregated signature is same message signed with given public keys.
+ * @param publicKeys
+ * @param messageHash
+ * @param signature
+ */
+export function verifyAggregate(publicKeys: Uint8Array[], messageHash: Uint8Array, signature: Uint8Array): boolean {
+  assert(publicKeys, "publicKey is null or undefined");
+  assert(messageHash, "messageHash is null or undefined");
+  assert(signature, "signature is null or undefined");
+  try {
+    return Signature
+      .fromCompressedBytes(signature)
+      .verifyAggregate(publicKeys, messageHash);
+  } catch (e) {
+    return false;
+  }
+}
+
+/**
  * Verifies if signature is list of message signed with corresponding public key.
  * @param publicKeys
  * @param messageHashes
  * @param signature
+ * @param fast Check if all messages are different
  */
 export function verifyMultiple(
   publicKeys: Uint8Array[],
   messageHashes: Uint8Array[],
   signature: Uint8Array,
+  fast = false
 ): boolean {
   assert(publicKeys, "publicKey is null or undefined");
   assert(messageHashes, "messageHash is null or undefined");
@@ -113,6 +134,7 @@ export function verifyMultiple(
       .verifyMultiple(
         publicKeys.map((key) => PublicKey.fromBytes(toBuffer(key))),
         messageHashes.map((m) => toBuffer(m)),
+        fast
       );
   } catch (e) {
     return false;
@@ -126,5 +148,6 @@ export default {
   aggregateSignatures,
   aggregatePubkeys,
   verify,
+  verifyAggregate,
   verifyMultiple
 };

src/signature.ts
@@ -11,6 +11,7 @@ export class Signature {
 
   protected constructor(value: SignatureType) {
     this.value = value;
+    assert(this.value.isValidOrder());
   }
 
   public static fromCompressedBytes(value: Uint8Array): Signature {
@@ -30,6 +31,15 @@ export class Signature {
     return new Signature(signature);
   }
 
+  public static aggregate(signatures: Signature[]): Signature {
+    const context = getContext();
+    const signature = new context.Signature();
+    signature.aggregate(signatures.map((sig) => sig.getValue()));
+    return new Signature(
+      signature
+    );
+  }
+
   public add(other: Signature): Signature {
     const agg = this.value.clone();
     agg.add(other.value);
@@ -42,14 +52,21 @@ export class Signature {
     return this.value;
   }
 
-  public verify(publicKey: PublicKey, message: Uint8Array): boolean {
-    return publicKey.verifyMessage(this, message);
+  public verifyAggregate(publicKey: Uint8Array[], message: Uint8Array): boolean {
+    return this.value.fastAggregateVerify(
+      publicKey.map((bytes) => PublicKey.fromBytes(bytes).getValue()),
+      message
+    );
   }
 
-  public verifyMultiple(publicKeys: PublicKey[], messages: Uint8Array[]): boolean {
-    return this.value.verifyAggregatedHashWithDomain(
+  public verifyMultiple(publicKeys: PublicKey[], messages: Uint8Array[], fast = false): boolean {
+    const msgs = Buffer.concat(messages);
+    if(!fast && !getContext().areAllMsgDifferent(msgs)) {
+      return false;
+    }
+    return this.value.aggregateVerifyNoCheck(
       publicKeys.map((key) => key.getValue()),
-      messages
+      msgs
     );
   }
 