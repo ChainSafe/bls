ChainSafe/lodestar

remove domain param from every method in bls

packages/bls/src/privateKey.ts
@@ -1,12 +1,11 @@
 import {SECRET_KEY_LENGTH} from "./constants";
 import assert from "assert";
-import {BLSSecretKey, Domain, bytes32} from "@chainsafe/eth2.0-types";
+import {BLSSecretKey, bytes32} from "@chainsafe/eth2.0-types";
 import {SecretKeyType} from "@chainsafe/eth2-bls-wasm";
 import {generateRandomSecretKey} from "@chainsafe/bls-keygen";
 import {getContext} from "./context";
 import {PublicKey} from "./publicKey";
 import {Signature} from "./signature";
-import {padLeft} from "./helpers/utils";
 
 export class PrivateKey {
 
@@ -51,9 +50,8 @@ export class PrivateKey {
   //   return Signature.fromValue(this.value.sign(message));
   // }
 
-  public signMessage(message: bytes32, domain: Domain): Signature {
-    domain = padLeft(domain, 8);
-    return Signature.fromValue(this.value.signHashWithDomain(Buffer.concat([message, domain])));
+  public signMessage(message: bytes32): Signature {
+    return Signature.fromValue(this.value.signHashWithDomain(Buffer.concat([message])));
   }
 
   public toPublicKey(): PublicKey {

packages/bls/src/publicKey.ts
@@ -49,8 +49,8 @@ export class PublicKey {
     return agg;
   }
 
-  public verifyMessage(signature: Signature, messageHash: bytes32, domain: Domain): boolean {
-    return this.value.verifyHashWithDomain(signature.getValue(), Buffer.concat([messageHash, domain]));
+  public verifyMessage(signature: Signature, messageHash: bytes32): boolean {
+    return this.value.verifyHashWithDomain(signature.getValue(), messageHash);
   }
 
   public toBytesCompressed(): BLSPubkey {

packages/bls/src/signature.ts
@@ -1,10 +1,10 @@
 import assert from "assert";
 import {FP_POINT_LENGTH} from "./constants";
-import {BLSSignature, Domain, bytes32} from "@chainsafe/eth2.0-types";
+import {BLSSignature, bytes32} from "@chainsafe/eth2.0-types";
 import {SignatureType} from "@chainsafe/eth2-bls-wasm";
 import {getContext} from "./context";
 import {PublicKey} from "./publicKey";
-import {EMPTY_SIGNATURE, padLeft} from "./helpers/utils";
+import {EMPTY_SIGNATURE} from "./helpers/utils";
 
 export class Signature {
 
@@ -43,16 +43,14 @@ export class Signature {
     return this.value;
   }
 
-  public verify(publicKey: PublicKey, message: bytes32, domain: Domain): boolean {
-    domain = padLeft(domain, 8);
-    return publicKey.verifyMessage(this, message, domain);
+  public verify(publicKey: PublicKey, message: bytes32): boolean {
+    return publicKey.verifyMessage(this, message);
   }
 
-  public verifyMultiple(publicKeys: PublicKey[], messages: bytes32[], domain: Domain): boolean {
-    domain = padLeft(domain, 8);
+  public verifyMultiple(publicKeys: PublicKey[], messages: bytes32[]): boolean {
     return this.value.verifyAggregatedHashWithDomain(
       publicKeys.map((key) => key.getValue()),
-      messages.map((message) => Buffer.concat([message, domain]))
+      messages
     );
   }
 

ChainSafe/bls

src/privateKey.ts
@@ -5,7 +5,6 @@ import {generateRandomSecretKey} from "@chainsafe/bls-keygen";
 import {getContext} from "./context";
 import {PublicKey} from "./publicKey";
 import {Signature} from "./signature";
-import {padLeft} from "./helpers/utils";
 
 export class PrivateKey {
 
@@ -50,9 +49,8 @@ export class PrivateKey {
   //   return Signature.fromValue(this.value.sign(message));
   // }
 
-  public signMessage(message: Uint8Array, domain: Uint8Array): Signature {
-    domain = padLeft(domain, 8);
-    return Signature.fromValue(this.value.signHashWithDomain(Buffer.concat([message, domain])));
+  public signMessage(message: Uint8Array): Signature {
+    return Signature.fromValue(this.value.signHashWithDomain(message));
   }
 
   public toPublicKey(): PublicKey {

src/publicKey.ts
@@ -48,8 +48,8 @@ export class PublicKey {
     return agg;
   }
 
-  public verifyMessage(signature: Signature, messageHash: Uint8Array, domain: Uint8Array): boolean {
-    return this.value.verifyHashWithDomain(signature.getValue(), Buffer.concat([messageHash, domain]));
+  public verifyMessage(signature: Signature, messageHash: Uint8Array): boolean {
+    return this.value.verifyHashWithDomain(signature.getValue(), messageHash);
   }
 
   public toBytesCompressed(): Buffer {

src/signature.ts
@@ -3,7 +3,7 @@ import {FP_POINT_LENGTH} from "./constants";
 import {SignatureType} from "@chainsafe/eth2-bls-wasm";
 import {getContext} from "./context";
 import {PublicKey} from "./publicKey";
-import {EMPTY_SIGNATURE, padLeft} from "./helpers/utils";
+import {EMPTY_SIGNATURE} from "./helpers/utils";
 
 export class Signature {
 
@@ -42,16 +42,14 @@ export class Signature {
     return this.value;
   }
 
-  public verify(publicKey: PublicKey, message: Uint8Array, domain: Uint8Array): boolean {
-    domain = padLeft(domain, 8);
-    return publicKey.verifyMessage(this, message, domain);
+  public verify(publicKey: PublicKey, message: Uint8Array): boolean {
+    return publicKey.verifyMessage(this, message);
   }
 
-  public verifyMultiple(publicKeys: PublicKey[], messages: Uint8Array[], domain: Uint8Array): boolean {
-    domain = padLeft(domain, 8);
+  public verifyMultiple(publicKeys: PublicKey[], messages: Uint8Array[]): boolean {
     return this.value.verifyAggregatedHashWithDomain(
       publicKeys.map((key) => key.getValue()),
-      messages.map((message) => Buffer.concat([message, domain]))
+      messages
     );
   }
 