b02cd11c969cde15a7de88657fa3c63830e1b5c1
dapplion <guillem.lion.benosa@gmail.com>
2020-11-30 22:05:19 +0000

    Benchmark noble-bls12-381
    f

--- test/benchmark/index.ts
+++ test/benchmark/index.ts
@@ -1,100 +1,92 @@
 import {runBenchmark} from "./runner";
 import {runForAllImplementations} from "../switch";
 import {IPublicKey, ISignature} from "../../src/interface";
-import {randomBytes} from "../../src/helpers";
+import {range, randomMessage} from "../util";
 
-runForAllImplementations((bls, implementation) => {
-  const aggCount = 30;
+const aggCount = 30;
 
-  // verify
+(async function () {
+  await runForAllImplementations(async (bls, implementation) => {
+    // verify
 
-  runBenchmark<{pk: IPublicKey; msg: Uint8Array; sig: ISignature}, boolean>({
-    id: `${implementation} verify`,
+    await runBenchmark<{pk: IPublicKey; msg: Uint8Array; sig: ISignature}, boolean>({
+      id: `${implementation} verify`,
 
-    prepareTest: () => {
-      const msg = randomMsg();
-      const sk = bls.SecretKey.fromKeygen();
-      const pk = sk.toPublicKey();
-      const sig = sk.sign(msg);
-      return {
-        input: {pk, msg, sig},
-        resultCheck: (valid) => valid === true,
-      };
-    },
-    testRunner: ({pk, msg, sig}) => {
-      return sig.verify(pk, msg);
-    },
-  });
-
-  // Fast aggregate
-
-  runBenchmark<{pks: IPublicKey[]; msg: Uint8Array; sig: ISignature}, boolean>({
-    id: `${implementation} verifyAggregate`,
-
-    prepareTest: () => {
-      const msg = randomMsg();
-      const dataArr = range(aggCount).map(() => {
+      prepareTest: () => {
+        const msg = randomMessage();
         const sk = bls.SecretKey.fromKeygen();
         const pk = sk.toPublicKey();
         const sig = sk.sign(msg);
-        return {pk, sig};
-      });
+        return {
+          input: {pk, msg, sig},
+          resultCheck: (valid) => valid === true,
+        };
+      },
+      testRunner: ({pk, msg, sig}) => {
+        return sig.verify(pk, msg);
+      },
+    });
 
-      const pks = dataArr.map((data) => data.pk);
-      const sig = bls.Signature.aggregate(dataArr.map((data) => data.sig));
+    // Fast aggregate
 
-      return {
-        input: {pks, msg, sig},
-        resultCheck: (valid) => valid === true,
-      };
-    },
-    testRunner: ({pks, msg, sig}) => {
-      return sig.verifyAggregate(pks, msg);
-    },
+    await runBenchmark<{pks: IPublicKey[]; msg: Uint8Array; sig: ISignature}, boolean>({
+      id: `${implementation} verifyAggregate`,
+
+      prepareTest: () => {
+        const msg = randomMessage();
+        const dataArr = range(aggCount).map(() => {
+          const sk = bls.SecretKey.fromKeygen();
+          const pk = sk.toPublicKey();
+          const sig = sk.sign(msg);
+          return {pk, sig};
+        });
+
+        const pks = dataArr.map((data) => data.pk);
+        const sig = bls.Signature.aggregate(dataArr.map((data) => data.sig));
+
+        return {
+          input: {pks, msg, sig},
+          resultCheck: (valid) => valid === true,
+        };
+      },
+      testRunner: ({pks, msg, sig}) => {
+        return sig.verifyAggregate(pks, msg);
+      },
+    });
+
+    // Aggregate pubkeys
+
+    await runBenchmark<IPublicKey[], void>({
+      id: `${implementation} aggregate pubkeys (${aggCount})`,
+
+      prepareTest: () => {
+        return {
+          input: range(aggCount).map(() => bls.SecretKey.fromKeygen().toPublicKey()),
+        };
+      },
+      testRunner: (pks) => {
+        bls.PublicKey.aggregate(pks);
+      },
+    });
+
+    // Aggregate sigs
+
+    await runBenchmark<ISignature[], void>({
+      id: `${implementation} aggregate signatures (${aggCount})`,
+
+      prepareTest: () => {
+        const msg = randomMessage();
+        const sigs = range(aggCount).map(() => {
+          const sk = bls.SecretKey.fromKeygen();
+          return sk.sign(msg);
+        });
+        return {
+          input: sigs,
+        };
+      },
+      testRunner: (sigs) => {
+        bls.Signature.aggregate(sigs);
+      },
+    });
   });
-
-  // Aggregate pubkeys
-
-  runBenchmark<IPublicKey[], void>({
-    id: `${implementation} aggregate pubkeys (${aggCount})`,
-
-    prepareTest: () => {
-      return {
-        input: range(aggCount).map(() => bls.SecretKey.fromKeygen().toPublicKey()),
-      };
-    },
-    testRunner: (pks) => {
-      bls.PublicKey.aggregate(pks);
-    },
-  });
-
-  // Aggregate sigs
-
-  runBenchmark<ISignature[], void>({
-    id: `${implementation} aggregate signatures (${aggCount})`,
-
-    prepareTest: () => {
-      const msg = randomMsg();
-      const sigs = range(aggCount).map(() => {
-        const sk = bls.SecretKey.fromKeygen();
-        return sk.sign(msg);
-      });
-      return {
-        input: sigs,
-      };
-    },
-    testRunner: (sigs) => {
-      bls.Signature.aggregate(sigs);
-    },
-  });
-});
-
-function range(n: number): number[] {
-  const nums: number[] = [];
-  for (let i = 0; i < n; i++) nums.push(i);
-  return nums;
-}
-
-function randomMsg(): Uint8Array {
-  return randomBytes(32);
-}
+})();
--- test/util.ts
+++ test/util.ts
@@ -7,3 +7,9 @@ export function randomMessage(): Uint8Array {
 export function getN<T>(n: number, getter: () => T): T[] {
   return Array.from({length: n}, () => getter());
 }
+
+export function range(n: number): number[] {
+  const nums: number[] = [];
+  for (let i = 0; i < n; i++) nums.push(i);
+  return nums;
+}
