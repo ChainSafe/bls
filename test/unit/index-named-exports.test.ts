2c938ddeed7e61e84cdf78e307631fa166e8c5a7
dapplion <guillem.lion.benosa@gmail.com>
2020-11-30 11:49:37 +0000

    Test named exports

---
+++ test/unit/index-named-exports.test.ts
@@ -0,0 +1,21 @@
+import {expect} from "chai";
+import {PrivateKey, PublicKey, Signature, init} from "../../src";
+
+describe("index named exports", () => {
+  it("Classes and methods should be defined", async () => {
+    await init("herumi");
+
+    const sk = PrivateKey.fromKeygen();
+    const msg = new Uint8Array(32);
+    const sig = sk.sign(msg);
+    const pk = sk.toPublicKey();
+    expect(verifyHelper(pk, sig, msg)).to.be.true;
+  });
+
+  /**
+   * Sample helper to test argument typing
+   */
+  function verifyHelper(pk: PublicKey, sig: Signature, msg: Uint8Array): boolean {
+    return sig.verify(pk, msg);
+  }
+});
