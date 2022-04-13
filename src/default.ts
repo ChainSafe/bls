a8aa89176847cb726124cccdd1a8fe9d0b499b21
Cayman <caymannava@gmail.com>
2022-04-13 13:33:59 -0500

    Tweak default export

--- src/default.ts
+++ src/default.ts
@@ -3,6 +3,6 @@ import {getImplementation} from "./getImplementation.js";
 // Thanks https://github.com/iliakan/detect-node/blob/master/index.esm.js
 const isNode = Object.prototype.toString.call(typeof process !== "undefined" ? process : 0) === "[object process]";
 
-export const bls = await getImplementation(isNode ? "blst-native" : "herumi");
+const bls = await getImplementation(isNode ? "blst-native" : "herumi");
+
 export default bls;
-export * from "./interface.js";
