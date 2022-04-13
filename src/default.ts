501f93e33338a72f7988e099d1c0da3ada6c2221
Cayman <caymannava@gmail.com>
2022-04-13 13:48:25 -0500

    Add herumi fallback in default export

--- src/default.ts
+++ src/default.ts
@@ -1,8 +1,14 @@
+import type {IBls} from "./interface.js";
 import {getImplementation} from "./getImplementation.js";
 
 // Thanks https://github.com/iliakan/detect-node/blob/master/index.esm.js
 const isNode = Object.prototype.toString.call(typeof process !== "undefined" ? process : 0) === "[object process]";
 
-const bls = await getImplementation(isNode ? "blst-native" : "herumi");
+let bls: IBls;
+try {
+  bls = await getImplementation(isNode ? "blst-native" : "herumi");
+} catch (e) {
+  bls = await getImplementation("herumi");
+}
 
 export default bls;
