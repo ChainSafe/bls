e95ae37056bc33f11a0169f7581836634e8aa773
matthewkeil <me@matthewkeil.com>
2024-04-10 23:29:16 +0700

    docs: add note to CoordType

--- src/types.ts
+++ src/types.ts
@@ -13,6 +13,17 @@ export enum PointFormat {
   uncompressed = "uncompressed",
 }
 
+/**
+ * NOTE: This MUST match the type used in @chainsafe/blst.  Do not use the one
+ * exported by that library though or it will mess up tree shaking.  Use the one
+ * below instead by MAKE SURE if you change this that it matches the enum values
+ * used by the native bindings in the base library!!!!  Better yet do not modify
+ * this unless you are ABSOLUTELY SURE you know what you are doing...
+ */
+/**
+ * CoordType allows switching between affine and jacobian version of underlying
+ * bls points.
+ */
 export enum CoordType {
   affine = 0,
   jacobian = 1,
