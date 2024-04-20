7afcd19853c248d840b0dc03734bf771b5727e96
matthewkeil <me@matthewkeil.com>
2024-04-20 22:51:22 +0700

    feat: update herumi classes to move herumi specific code into class methods

--- src/herumi/signature.ts
+++ src/herumi/signature.ts
@@ -1,8 +1,8 @@
 import type {SignatureType} from "bls-eth-wasm";
 import {getContext} from "./context.js";
 import {PublicKey} from "./publicKey.js";
-import {bytesToHex, concatUint8Arrays, hexToBytes, isZeroUint8Array} from "../helpers/index.js";
-import {SignatureSet, PointFormat, Signature as ISignature, CoordType} from "../types.js";
+import {bytesToHex, concatUint8Arrays, hexToBytes, isZeroUint8Array, validateBytes} from "../helpers/index.js";
+import {SignatureSet, PointFormat, Signature as ISignature, CoordType, PublicKeyArg, SignatureArg} from "../types.js";
 import {EmptyAggregateError, InvalidLengthError, InvalidOrderError} from "../errors.js";
 import {SIGNATURE_LENGTH_COMPRESSED, SIGNATURE_LENGTH_UNCOMPRESSED} from "../constants.js";
 
@@ -41,22 +41,24 @@ export class Signature implements ISignature {
     return this.fromBytes(hexToBytes(hex));
   }
 
-  static aggregate(signatures: Signature[]): Signature {
+  static aggregate(signatures: SignatureArg[]): Signature {
     if (signatures.length === 0) {
       throw new EmptyAggregateError();
     }
 
     const context = getContext();
-    const signature = new context.Signature();
-    signature.aggregate(signatures.map((sig) => sig.value));
-    return new Signature(signature);
+    const agg = new context.Signature();
+    agg.aggregate(signatures.map(Signature.convertToSignatureType));
+    return new Signature(agg);
   }
 
   static verifyMultipleSignatures(sets: SignatureSet[]): boolean {
+    if (!sets) throw Error("sets is null or undefined");
+
     const context = getContext();
     return context.multiVerify(
-      sets.map((s) => s.publicKey.value),
-      sets.map((s) => s.signature.value),
+      sets.map((s) => PublicKey.convertToPublicKeyType(s.publicKey)),
+      sets.map((s) => Signature.convertToSignatureType(s.signature)),
       sets.map((s) => s.message)
     );
   }
@@ -70,20 +72,40 @@ export class Signature implements ISignature {
     return Signature.verifyMultipleSignatures(sets);
   }
 
-  verify(publicKey: PublicKey, message: Uint8Array): boolean {
-    return publicKey.value.verify(this.value, message);
+  static convertToSignatureType(signature: SignatureArg): SignatureType {
+    let sig: Signature;
+    if (signature instanceof Uint8Array) {
+      validateBytes(signature, "signature");
+      sig = Signature.fromBytes(signature);
+    } else {
+      // need to cast to herumi key instead of ISignature
+      sig = signature as Signature;
+    }
+    return sig.value;
   }
 
-  verifyAggregate(publicKeys: PublicKey[], message: Uint8Array): boolean {
-    return this.value.fastAggregateVerify(
-      publicKeys.map((key) => key.value),
-      message
-    );
+  verify(publicKey: PublicKeyArg, message: Uint8Array): boolean {
+    validateBytes(message, "message");
+    return PublicKey.convertToPublicKeyType(publicKey).verify(this.value, message);
   }
 
-  verifyMultiple(publicKeys: PublicKey[], messages: Uint8Array[]): boolean {
+  verifyAggregate(publicKeys: PublicKeyArg[], message: Uint8Array): boolean {
+    validateBytes(message, "message");
+    return this.value.fastAggregateVerify(publicKeys.map(PublicKey.convertToPublicKeyType), message);
+  }
+
+  verifyMultiple(publicKeys: PublicKeyArg[], messages: Uint8Array[]): boolean {
+    // TODO: (@matthewkeil) this was in the verifyMultiple free function but was moved here for herumi. blst-native
+    //       does this check but throws error instead of returning false.  Need to double check spec on which is
+    //       correct handling
+    if (publicKeys.length === 0 || publicKeys.length != messages.length) {
+      return false;
+    }
+
+    validateBytes(messages, "message");
+
     return this.value.aggregateVerifyNoCheck(
-      publicKeys.map((key) => key.value),
+      publicKeys.map(PublicKey.convertToPublicKeyType),
       concatUint8Arrays(messages)
     );
   }
